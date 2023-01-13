43ac5d4b0f6c2248f30c5cf958c0045836e2e3ef
James Fogwill <james@robfogwill.com>
2023-01-13 16:24:12 +0000

    revise

--- Tasks/TASK-203-PortOut/main.cpp
+++ Tasks/TASK-203-PortOut/main.cpp
@@ -4,18 +4,20 @@
 #define TRAF_GRN1_PIN PC_6
 #define TRAF_YEL1_PIN PC_3
 #define TRAF_RED1_PIN PC_2
-#define LEDMASK 0b0000000001001100
+#define LEDMASK  0b0000000001001100
+#define LEDMASKB 0b0100000010000001
 // Objects
 //BusOut leds(TRAF_RED1_PIN, TRAF_YEL1_PIN, TRAF_GRN1_PIN);
 PortOut leds(PortC, LEDMASK);
+PortOut ledsB(PortB, LEDMASKB);
 
 int main()
 {
     while (true) {
-        leds = 0;   //Binary 000
+        leds && ledsB ^ 1;   //Binary 000
         wait_us(500000);
-        leds = 0xFF;   //Binary 11111111
-        wait_us(500000);    
+           //Binary 11111111
+        
     }
 }
 
